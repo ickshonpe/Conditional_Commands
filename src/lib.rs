00eecba36b429d7b2f455824debfd279d195afc9
Ickshonpe <david.curthoys@googlemail.com>
2022-05-30 11:04:22 +0100

    added some doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -4,52 +4,55 @@ use bevy::prelude::*;
 pub trait ConditionalEntityCommandsExt {
     fn insert_if<C: Component>(
         &mut self,
-        cond: bool,
+        condition: bool,
         component: C,
     ) -> &mut Self;
 
     fn insert_bundle_if<B: Bundle>(
         &mut self,
-        cond: bool,
+        condition: bool,
         component: B,
     ) -> &mut Self;
 
     fn with_children_if(
         &mut self,
-        cond: bool,
+        condition: bool,
         child_builder: impl FnOnce(&mut ChildBuilder)
     ) -> &mut Self;
 }
 
 impl ConditionalEntityCommandsExt for EntityCommands<'_, '_, '_> {
+    /// if `condition`, add a [`Component`] to the entity
     fn insert_if<C: Component>(
         &mut self,
-        cond: bool,
+        condition: bool,
         component: C,
     ) -> &mut Self {
-        if cond {
+        if condition {
             self.insert(component);
         }
         self
     }
 
+    /// if `condition`, add a [`Bundle`] to the entity
     fn insert_bundle_if<B: Bundle>(
         &mut self,
-        cond: bool,
+        condition: bool,
         bundle: B,
     ) -> &mut Self {
-        if cond {
+        if condition {
             self.insert_bundle(bundle);
         }
         self
     }
 
+    /// if `condition`, create a [`ChildBuilder`] for the entity
     fn with_children_if(
         &mut self,
-        cond: bool,
+        condition: bool,
         child_builder: impl FnOnce(&mut ChildBuilder)
     ) -> &mut Self {
-        if cond {
+        if condition {
             self.with_children(child_builder);
         }
         self
